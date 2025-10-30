7675de0f1a180343dfa0a96555b83f96005b49b1
fouadelozery <fouadelozery7@gmail.com>
2025-10-30 05:33:31 +0300

    update get bestseller

--- lib/features/home/view/get_current_view.dart
+++ lib/features/home/view/get_current_view.dart
@@ -4,9 +4,8 @@ import 'package:e_commerce/features/home/view/widgets/home_view.dart';
 
 List<Widget> getCurrentView() {
   return const [
-    HomeView(),   // الرئيسية
-    ProductView(), // المنتجات
-    Center(child: Text("عربة التسوق")), // Temporary Cart Screen
-    Center(child: Text("الحساب")),      // Temporary Profile Screen
+    HomeView(), 
+    ProductView(), 
+    Center(child: Text("عربة التسوق")), 
   ];
 }
